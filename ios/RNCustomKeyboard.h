2f1b8dd0610bde992e9e8f9632af13b0b4ec1a3c
Aevit <Aevitx@gmail.com>
2017-11-10 03:19:32 -0600

    adapt RN 0.48

--- ios/RNCustomKeyboard.h
+++ ios/RNCustomKeyboard.h
@@ -1,5 +1,9 @@
 
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#elif __has_include("RCTBridgeModule.h")
 #import "RCTBridgeModule.h"
+#endif
 
 @interface RNCustomKeyboard : NSObject <RCTBridgeModule>
 
