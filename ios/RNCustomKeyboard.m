2f3690b93a0857ff7a13acd8e4d8b850f95b2dbb
Aevit <Aevitx@gmail.com>
2017-11-16 02:56:10 -0600

    0.50 RCTTextField doesn't inherit from UITextField

--- ios/RNCustomKeyboard.m
+++ ios/RNCustomKeyboard.m
@@ -24,7 +24,7 @@ RCT_EXPORT_METHOD(install:(nonnull NSNumber *)reactTag withType:(nonnull NSStrin
   ];
   inputView.frame = CGRectMake(0, 0, 0,252);
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   view.inputView = inputView;
   [view reloadInputViews];
 }
@@ -32,20 +32,20 @@ RCT_EXPORT_METHOD(install:(nonnull NSNumber *)reactTag withType:(nonnull NSStrin
 RCT_EXPORT_METHOD(uninstall:(nonnull NSNumber *)reactTag)
 {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   view.inputView = nil;
   [view reloadInputViews];
 }
 
 RCT_EXPORT_METHOD(insertText:(nonnull NSNumber *)reactTag withText:(NSString*)text) {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   [view replaceRange:view.selectedTextRange withText:text];
 }
 
 RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag) {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   UITextRange* range = view.selectedTextRange;
   if ([view comparePosition:range.start toPosition:range.end] == 0) {
     range = [view textRangeFromPosition:[view positionFromPosition:range.start offset:-1] toPosition:range.start];
@@ -55,7 +55,7 @@ RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag) {
 
 RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag) {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   UITextRange* range = view.selectedTextRange;
   if ([view comparePosition:range.start toPosition:range.end] == 0) {
     range = [view textRangeFromPosition:view.beginningOfDocument toPosition:[view positionFromPosition: range.start offset: 0]];
@@ -65,7 +65,7 @@ RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag) {
 
 RCT_EXPORT_METHOD(moveLeft:(nonnull NSNumber *)reactTag) {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   UITextRange* range = view.selectedTextRange;
   UITextPosition* position = range.start;
 
@@ -78,7 +78,7 @@ RCT_EXPORT_METHOD(moveLeft:(nonnull NSNumber *)reactTag) {
 
 RCT_EXPORT_METHOD(moveRight:(nonnull NSNumber *)reactTag) {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+  view = [self getRealTextView:view];
   UITextRange* range = view.selectedTextRange;
   UITextPosition* position = range.end;
 
@@ -91,10 +91,28 @@ RCT_EXPORT_METHOD(moveRight:(nonnull NSNumber *)reactTag) {
 
 RCT_EXPORT_METHOD(switchSystemKeyboard:(nonnull NSNumber*) reactTag) {
   UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
+  view = [self getRealTextView:view];
   UIView* inputView = view.inputView;
   view.inputView = nil;
   [view reloadInputViews];
   view.inputView = inputView;
+}
+
+- (UITextView*)getRealTextView:(UITextView*)reactView {
+    if ([self canInputText:reactView]) {
+        return reactView;
+    }
+    // RN 0.50 后 RCTTextField 不是继承自 UITextField 了，多包了一层，这里遍历一下去查找
+    for (UITextView *aView in reactView.subviews) {
+        if ([self canInputText:aView]) {
+            return (UITextView*)aView;
+        }
+    }
+    return nil;
+}
+
+- (BOOL)canInputText:(UIView*)view {
+    return [view isKindOfClass:[UITextField class]] || [view isKindOfClass:[UITextView class]];
 }
 
 @end
